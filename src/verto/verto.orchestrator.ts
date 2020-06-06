1344b2f2c5660612cd540c6340f48ec718e62974
Mathieu Delacroix <mathieu.delacroix.fr@gmail.com>
2020-06-06 21:22:04 +0200

    fix received call

--- src/verto/verto.orchestrator.ts
+++ src/verto/verto.orchestrator.ts
@@ -132,7 +132,7 @@ export class VertoOrchestrator {
         const call = new Call(
           message.params.callID,
           this.verto,
-          message.params.verto_h_originalCallerIdNumber,
+          message.params.caller_id_number,
           this.verto.getLogin(),
           pc,
           'inbound',
